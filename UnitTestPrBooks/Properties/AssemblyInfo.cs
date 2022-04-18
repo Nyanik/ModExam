357de57a887b9d253278f2aa4d4be6fc24893b04
Никита Мелузов <nikita.meluzov76@gmail.com>
2022-04-18 13:09:10 +0300

    оп

---
+++ UnitTestPrBooks/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPrBooks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPrBooks")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef8568d4-bc40-4f03-9989-42248663d77e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestPrBooks/UnitTestDis.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WSDiscount;
+
+namespace UnitTestPrBooks
+{
+    [TestClass]
+    public class UnitTestDis
+    {
+        [TestMethod]
+        public void Check_Discoiunt005()
+        {
+            int rezult = 1900;
+
+            Libr SD = new Libr();
+            int actual = SD.SumDis(2000, 0.05);
+
+            Assert.AreEqual(rezult, actual);
+        }
+        [TestMethod]
+        public void Check_Discoiunt01()
+        {
+            int rezult = 1800;
+
+            Libr SD = new Libr();
+            int actual = SD.SumDis(2000, 0.1);
+
+            Assert.AreEqual(rezult, actual);
+        }
+        [TestMethod]
+        public void Check_Discoiunt015()
+        {
+            int rezult = 1700;
+
+            Libr SD = new Libr();
+            int actual = SD.SumDis(2000, 0.15);
+
+            Assert.AreEqual(rezult, actual);
+        }
+    }
+}
